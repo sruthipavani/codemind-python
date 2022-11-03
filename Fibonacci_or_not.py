ed7d61e2e6871ebb69ccbd8df9be22a9d6b8b152
sruthipavani <84573488+sruthipavani@users.noreply.github.com>
2022-11-03 10:18:43 +0530

    def fib_seq(n):     a,b=0,1     if a==n or b==n:         return True     c=a+b     while True:         if c==n:             return True         if c>n:             return False         a=b         b=c         c=a+b n=int(input()) res=fib_seq(n) print(res)

---
+++ Fibonacci_or_not.py
@@ -0,0 +1,18 @@
+def fib_seq(n):
+    a,b=0,1
+    if a==n or b==n:
+        return True
+    c=a+b
+    while True:
+        if c==n:
+            return True
+        if c>n:
+            return False
+        a=b
+        b=c
+        c=a+b
+n=int(input())
+res=fib_seq(n)
+print(res)
+            
+        
